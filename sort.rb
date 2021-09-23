d1ac4871474fc5c490033cf04420ed6b4abce472
JAMChannel <pon244tt@gmail.com>
2021-09-24 02:48:24 +0900

    完成

--- sort.rb
+++ sort.rb
@@ -3,13 +3,8 @@ require 'byebug'
 def bubble_sort(number)
   num = number.count  
   num.times do |n| 
-    # 比較する位置を1つずつ左にずらすため、「- 1 」している
     (num - 1 - n).times do |f|
-      # if number[j] > number[j + 1]       # 比較（隣）
-        # byebug 
-         number[f+1],number[f] = number[f],number[f+1] if number[f] > number[f+1]
-        # byebug
-      # end
+      number[f+1],number[f] = number[f],number[f+1] if number[f] < number[f+1]
     end
   end
   number
